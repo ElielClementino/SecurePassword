7233ba51876e60331e45147663528ae815274ce7
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-12-06 02:44:17 -0300

    solve problems of django and docker not finding env configurations

--- core/settings.py
+++ core/settings.py
@@ -11,20 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Get env file to config the project
+env_file = os.path.join(BASE_DIR, ".env")
+load_dotenv(env_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-n2@@m)ec=kfd3ptezu%e!l&_f57q*2(aoba-u_")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
@@ -78,11 +81,11 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST"),
-        "PORT": config("POSTGRES_PORT"),
+        "NAME": os.getenv("POSTGRES_DB", "securepassword"),
+        "USER": os.getenv("POSTGRES_USER", "securepassword"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "securepassword"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
     }
 }
 
