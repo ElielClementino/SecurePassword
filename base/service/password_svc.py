e742d661ddae9b28446e72b888f5ecc9ecb604aa
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-12-21 22:34:17 -0300

    Add shell_plus extension, and pydantic

--- base/service/password_svc.py
+++ base/service/password_svc.py
@@ -6,4 +6,3 @@ from base.models import SecurePassword
 def create_password(data: passwordForm) -> Dict:
     created_password = SecurePassword.objects.create(name=data.name, password=data.password)
     return created_password
-
