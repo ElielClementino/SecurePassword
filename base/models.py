b408580ade6bdb277668e7f5aed55d52a7b57dc6
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-12-21 22:31:43 -0300

    changed group relationship to many to many

--- base/models.py
+++ base/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class SecurePassword(models.Model):
+    # user = models.ForeignKey(User, related_name="user_password", on_delete=models.CASCADE, null=False, blank=True) # still thinking if it's on the right place
+    name = models.CharField(max_length=240, null=True, blank=True)
+    password = models.TextField(null=False, blank=False)
 
 class Group(models.Model):
     name = models.CharField(max_length=120, null=True, blank=True)
-
-class SecurePassword(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group_password", null=True, blank=True)
-    name = models.CharField(max_length=240, null=True, blank=True)
-    password = models.TextField(null=False, blank=False)
+    password = models.ManyToManyField(SecurePassword, on_delete=models.CASCADE, related_name="group_password", null=True, blank=True)
